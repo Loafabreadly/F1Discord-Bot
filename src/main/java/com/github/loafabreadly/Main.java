3c4a7779f2fadac2b44dc6fdd58db969c60b18a5
Brad S <birdgeekthree@gmail.com>
2023-02-27 13:22:18 -0700

    Removing debug log + adding first slash

--- src/main/java/com/github/loafabreadly/Main.java
+++ src/main/java/com/github/loafabreadly/Main.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.interaction.SlashCommand;
 
 public class Main {
 
@@ -33,10 +34,14 @@ public class Main {
 
             //Main message event listener
             api.addMessageCreateListener(event -> {
-                if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                    event.getChannel().sendMessage("Pong!");
+                if (event.getMessageContent().equalsIgnoreCase("!nico")) {
+                    event.getChannel().sendMessage(":nicooo:");
                 }
             });
+
+            SlashCommand pingCmd = SlashCommand.with("ping", "Returns Pong")
+                    .createGlobal(api)
+                    .join();
         }
         catch (Exception e) {
             logger.error(e.getMessage());
